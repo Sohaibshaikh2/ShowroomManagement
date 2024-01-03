1feaf6009462c2595caa3719d1193e125b6d296f
Muhammad Sohaib Shaikh <ss0894757@gmail.com>
2024-01-03 21:09:11 +0500

    FirstCommit

---
+++ ShowRoomMangement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShowRoomMangement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
